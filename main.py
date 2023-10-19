aa1479e147439f4e5ed9c683db09fcd9dc0739ef
natalya tomova <natalyayurevna19@gmail.com>
2023-10-19 10:07:21 +1000

    Добавлен цикл while при взаимодействии с пользователем, добавлены варианты вывода информации по вакансиям.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
 
     user_choice = input('Выберите вариант поиска вакансий:\n'
                         '1 - по указанным Вами компаниям, \n'
-                        '2 - среди 10 лучших IT-компаний по рейтингу Хабр Карьеры:\n')
+                        '2 - среди 11 лучших IT-компаний по рейтингу Хабр Карьеры:\n')
     if user_choice == '1':
         employer_names = input('Введите названия интересующих вас компаний '
                                'через запятую: ').strip().split(', ')
@@ -35,22 +35,28 @@ def main():
     db_manager.add_vacancies_to_db(vacancies)
     print("Таблица vacancies успешно заполнена")
 
-    user_choice = input('Выберите дальнейшее действие:\n'
-                        '1 - Показать найденных работодателей и их вакансии,\n'
-                        '2 - Показать вакансии с зарплатой выше средней,\n'
-                        '3 - Вывести вакансии по ключевому слову в названии\n'
-                        '0 - Выйти\n')
-    if user_choice == '1':
-        db_manager.get_companies_and_vacancies_count()
-        db_manager.get_all_vacancies()
-    elif user_choice == '2':
-        db_manager.get_avg_salary()
-        db_manager.get_vacancies_with_higher_salary()
-    elif user_choice == '3':
-        key_word = input('Ключевое слово для поиска вакансий: ')
-        db_manager.get_vacancies_with_keyword(key_word)
-    elif user_choice == '0':
-        quit()
+    while True:
+        print()
+        user_choice = input('Выберите дальнейшее действие:\n'
+                            '1 - Показать найденных работодателей и их вакансии,\n'
+                            '2 - Показать все найденные вакансии,\n'
+                            '3 - Вывести среднюю зарплату по вакансиям,\n'
+                            '4 - Показать вакансии с зарплатой выше средней,\n'
+                            '5 - Вывести вакансии по ключевому слову в названии,\n'
+                            '0 - Выйти\n')
+        if user_choice == '1':
+            db_manager.get_companies_and_vacancies_count()
+        elif user_choice == '2':
+            db_manager.get_all_vacancies()
+        elif user_choice == '3':
+            db_manager.get_avg_salary()
+        elif user_choice == '4':
+            db_manager.get_vacancies_with_higher_salary()
+        elif user_choice == '5':
+            key_word = input('Ключевое слово для поиска вакансий: ')
+            db_manager.get_vacancies_with_keyword(key_word)
+        elif user_choice == '0':
+            break
 
 
 if __name__ == '__main__':
